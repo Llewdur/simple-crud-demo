87af724b501f750024ec1350453ddef281d6cf42
Llewellyn <support@zekini.com>
2021-01-28 04:34:00 +0200

    Add menu link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <br>
+                    <a href='{!! url('/languages-ajax'); !!}'>Languages</a>
                 </div>
             </div>
         </div>
